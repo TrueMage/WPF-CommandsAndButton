0e12617119dc12689f505c6ad51a0ba752648ba8
Andrii Hromanchuk <73118147+TrueMage@users.noreply.github.com>
2023-12-17 16:37:27 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_CommandsAndButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ChangeColorExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(e.Parameter as string));
+        }
+    }
+}
